58b65b5eea3920cc8c9f59cc8547383d6904002f
laxmi13579 <laxmi.yadav370515@gmail.com>
2024-06-02 13:01:16 +0530

    views: update HttpResponse

--- blogapp/views.py
+++ blogapp/views.py
@@ -16,7 +16,7 @@ def blog_list(request):
     return Response(serializers.data)
 
 def hello_world(request):
-    return HttpResponse('Hello World!!')
+    return HttpResponse('Hello World, Laxmi Yadav!!')
 
 def main_page(request):
     return render(request, 'main.html', {'name' : 'Laxmi Narayan yadav'})
